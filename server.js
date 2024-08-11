4646213024115431a7474f63922b114d2d017468
Amrit-786 <amritpalsingh9599123@gmail.com>
2024-08-11 18:28:33 +0530

    deploying backend

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ static: false });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
+
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}));
+
 server.use(router);
 
 server.listen(port, () => {
