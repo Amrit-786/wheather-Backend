f2e9de18a345a88e5a8b25574bfb6045d9d7c278
Amrit-786 <amritpalsingh9599123@gmail.com>
2024-08-12 13:50:18 +0530

    deploying backend

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults({ static: false });
+const middlewares = jsonServer.defaults({
+  static: false,
+  bodyParser: true
+});
 
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
+});
 
-server.use(router);
+server.use('/api', router);
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
